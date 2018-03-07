dab0f5e06b1dc8bb5aa6b4f3ce46df80586b9977
Manish Singh <manisin4@cisco.com>
2018-03-07 21:33:50 +0530

    Complete Application Developed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import {SuccessComponent} from './success/success.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/create',
+    pathMatch: 'full'
+  },
+  {
+    path: 'create',
+    component: CreateAccountComponent
+  },
+  {
+    path: 'success',
+    component: SuccessComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { SuccessComponent } from './success/success.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateAccountComponent,
+    SuccessComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
